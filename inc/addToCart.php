cd7e47f589a7fb8bccfdb81b3321fb6e1c6a9347
blazek1ng <36832489+blazek1ng@users.noreply.github.com>
2018-12-05 16:36:39 -0800

    Update addToCart.php

--- inc/addToCart.php
+++ inc/addToCart.php
@@ -10,11 +10,22 @@
     }
     
     if(array_key_exists($productId, $_SESSION['cart'])){
+        // If already exists in cart, do not add. I need to add some visual identifier
         header('Location: ../shop.php?action=exists&id=' . $productId);
     } else {
+        // Add to cart, stored in variable $_SESSION['cart']
         $_SESSION['cart'][$productId]=$cart_item;
         header('Location: ../shop.php');
     }
     
+
+    // Store the added product IDs in an array
+
+    // $ids = array();
+    // foreach($_SESSION['cart'] as $id=>$value){
+    //     array_push($ids, $id);
+    // }
+
+    // $ids contains product IDs ($ids = [2,7,8,4])
 
 ?>
