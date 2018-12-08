4a4902b0ae49baf650e2dd69e0036c1e0de34f5c
blazek1ng <36832489+blazek1ng@users.noreply.github.com>
2018-12-08 15:11:43 -0800

    Update index.php

--- index.php
+++ index.php
@@ -3,8 +3,9 @@
     include 'inc/header.php';
 ?>
 
-  <div class='container'>
-      Home Page
+  <div class='container text-center'>
+      <h1>Welcome to the Emporium</h1>
+      <img src='img/vhs.jpg' style='padding:30px;'>
   </div>
 <?php
     include 'inc/footer.php';
