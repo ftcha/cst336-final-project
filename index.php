3350256c22aad96c6e8f223c584832fc881c17e7
blazek1ng <36832489+blazek1ng@users.noreply.github.com>
2018-12-10 15:44:32 -0800

    Update index.php with Enter button

--- index.php
+++ index.php
@@ -6,6 +6,8 @@
   <div class='container text-center'>
       <h1>Welcome to the Emporium</h1>
       <img src='img/vhs.jpg' style='padding:30px;'>
+      <br>
+      <button type="submit" class="btn btn-lg" onclick="location.href='shop.php';" style='margin-bottom:20px;'>Enter</button>
   </div>
 <?php
     include 'inc/footer.php';
