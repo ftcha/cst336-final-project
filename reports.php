6ba5ba0d0e1bfe7ca7a7212919828463709fcc6c
Adam Irby <airby@csumb.edu>
2018-12-11 04:01:05 +0000

    Fixed inventory report name on main report page

--- reports.php
+++ reports.php
@@ -10,7 +10,7 @@
 <div class="container">
     <h3>Reports</h3>
     <ul class="nav nav-tabs">
-        <li><a href="report1.php">Average Price</a></li>
+        <li><a href="report1.php">Inventory Report</a></li>
         <li><a href="report2.php">Transaction Report</a></li>
         <li><a href="report3.php">Transaction Detail Report</a></li>
     </ul>
